cc0cb594ba5338c8476835645202764d0aa8ad66
MarcMocker <MarcMocker@github.com>
2022-06-27 10:49:40 +0200

    first commit

---
+++ SlashCommands/info/test.js
@@ -0,0 +1,15 @@
+const { CommandInteraction, Client} = require("discord.js");
+
+module.exports = {
+    name: "test",
+    description: "this is a testing command",
+
+    /**
+     * @param {Clinet} client
+     * @param {CommandInteraction} interaction
+     * @param {String[]} args
+     */
+    run: async (client, interaction, args) => {
+        interaction.reply({ content: "Hello World" });
+    }
+}
---
+++ readme.md
@@ -0,0 +1,13 @@
+Handler for discord.js version 13
+
+`npm install` - install all required dependancies
+
+`node index.js` - start the project
+
+Only open pull requests for bug fixes, I want to keep the code as clean as possible, thanks!
+
+
+## deploying the bot
+
+The bot tries to access a file `config.json` take the `config.json.example` file and fill in the
+necessary data as well as rename it.
